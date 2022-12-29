eb4b06c4a47c15a4cc29d46958f41c83d596bb23
wisie <wisnia501@gmail.com>
2022-12-29 08:36:46 +0000

    fix mr beast gif

--- responses.py
+++ responses.py
@@ -8,17 +8,16 @@ def get_response(message: str) -> str:
     # this checks whether the message is a command 
     if message[:2] == 'm!':
         message = message[2:]
+        # joke command: returns a random joke taken from this API below
         if message == 'joke':
             response = requests.get(r"https://official-joke-api.appspot.com/random_joke")
-            tt = json.loads(response.text)
-            return f"{tt['setup']}\n{tt['punchline']}"
-
+            joke = json.loads(response.text)
+            return f"{joke['setup']}\n\n...{joke['punchline']}"
 
     phrases_list = {
         'baller': 'https://cdn.discordapp.com/attachments/654594751433146378/1033767835199078431/41ce545e2c093bcbf4461c459b4f4dec.mp4',
-        'beast': 'https://images-ext-1.discordapp.net/external/rgJ32du_uD-2yeC6gmKyjRZACFPZjBgOBY1bm-_eGXg/https/media.tenor.com/IIXy6CqR5l8AAAPo/mrbeast-ytpmv.mp4'
+        'beast': 'https://media.discordapp.net/attachments/1034298084076834869/1057939585050615848/mrbeast-ytpmv.gif',
     }
-
     for phrase in phrases_list.keys():
         if phrase in message:
             return phrases_list[phrase]
