4dfa16eb43dc13889f258ac4e899e491a765ce66
wisie <wisnia501@gmail.com>
2023-01-06 18:43:13 +0000

    add m!help command

--- responses.py
+++ responses.py
@@ -13,7 +13,12 @@ def get_response(message: str) -> str:
             response = requests.get(r"https://official-joke-api.appspot.com/random_joke")
             joke = json.loads(response.text)
             return f"{joke['setup']}\n\n...{joke['punchline']}"
+        elif message == 'help':
+            return '''```m!help -> Prints this page
+m!joke -> Prints a random joke (taken from various APIs)
+...with more to come```'''
 
+    # if message not recognized as a command, will search for keywords to respond to
     phrases_list = {
         'baller': 'https://cdn.discordapp.com/attachments/654594751433146378/1033767835199078431/41ce545e2c093bcbf4461c459b4f4dec.mp4',
         'beast': 'https://media.discordapp.net/attachments/1034298084076834869/1057939585050615848/mrbeast-ytpmv.gif',
